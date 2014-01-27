f9ba72ee14df53fab6a56779fbba92726a49e4e4
Robert Brockie <robertbrockie@gmail.com>
2014-01-27 17:25:06 -0500

    Added a bit of style to the interface

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,15 +31,19 @@
 	</head>
 	<body>
 
-	<h1>Dialpad Translator</h1>
-	<form action="index.php" method="post">
-		<input type="text" name="word" value="<?= $word ?>" />
-		<input type="submit" value="Translate"/>
-	</form>
+		<div class="container">
+			<h1>Dialpad Translator</h1>
 
-	<?php if ($word) { ?>
-		<h3><?= $word ?> => <?= $number ?></h3>
-	<?php } ?>
+			<form action="index.php" method="post">
+				<p><input class="form-control" type="text" name="word" value="<?= $word ?>" /></p>
+				<p><input class="btn btn-primary btn-lg" type="submit" value="Translate"/></p>
+			</form>
 
+			<?php if ($word) { ?>
+				<div class="alert alert-success">
+					<span><?= $number ?></span>
+				</div>
+			<?php } ?>
+		</div>
 	</body>
 </html>
